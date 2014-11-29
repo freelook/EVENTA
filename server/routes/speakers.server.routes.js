d5095d84a50ce1441a90244af0b0c3a9ac9ffd04
Mykola Turunov <nick.turunov@gmail.com>
2014-11-29 13:52:13 +0200

    Speakers data

--- server/routes/speakers.server.routes.js
+++ server/routes/speakers.server.routes.js
@@ -10,13 +10,8 @@ var speakers = require('../../server/controllers/speakers');
 module.exports = function(app) {
     // Person Routes
     app.route('/speakers')
-        .get(speakers.list)
-        .post(speakers.create);
+        .get(speakers.list);
 
-    app.route('/speakers/:speakerId')
-        .get(speakers.read)
-        .put(speakers.update)
-        .delete(speakers.delete);
 
     // Finish by binding the speaker middleware
     app.param('speakerId', speakers.speakerByID);
