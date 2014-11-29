6de0d8c07b91641d0e2eef25773eb747dfe62d34
Olena Sovyn <olenasovyn@gmail.com>
2014-11-29 10:44:48 +0200

    Database url added

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://localhost/eventa-dev',
+	db: 'mongodb://eventa:eventa12345@ds053090.mongolab.com:53090/eventa',
 	baseURL: 'localhost:' + process.env.PORT || 3000 + '/',
 	app: {
 		title: 'EVENTA - Development Environment'
