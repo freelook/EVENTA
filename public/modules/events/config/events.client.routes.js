8e255744f65b030d2cb60baf7e2e3ac9e6605917
freelook <freelook@mail.ua>
2014-11-30 01:54:31 +0200

    mobile build

--- public/modules/events/config/events.client.routes.js
+++ public/modules/events/config/events.client.routes.js
@@ -7,19 +7,19 @@ angular.module('events').config(['$stateProvider',
 		$stateProvider.
 		state('listEvents', {
 			url: '/events',
-			templateUrl: '/modules/events/views/list-events.client.view.html'
+			templateUrl: 'modules/events/views/list-events.client.view.html'
 		}).
 		state('createEvent', {
 			url: '/events/create',
-			templateUrl: '/modules/events/views/create-event.client.view.html'
+			templateUrl: 'modules/events/views/create-event.client.view.html'
 		}).
 		state('viewEvent', {
 			url: '/events/:eventId',
-			templateUrl: '/modules/events/views/view-event.client.view.html'
+			templateUrl: 'modules/events/views/view-event.client.view.html'
 		}).
 		state('editEvent', {
 			url: '/events/:eventId/edit',
-			templateUrl: '/modules/events/views/edit-event.client.view.html'
+			templateUrl: 'modules/events/views/edit-event.client.view.html'
 		});
 	}
 ]);
